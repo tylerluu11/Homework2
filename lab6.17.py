1393aefa2777bf225e504a968eeb2abba96bfde3
tylerluu11 <123343265+tylerluu11@users.noreply.github.com>
2023-03-07 16:41:55 -0600

    lab 6.17

---
+++ lab6.17.py
@@ -0,0 +1,9 @@
+rewrite = str(input())
+rewrite = rewrite.replace('i','!')
+rewrite = rewrite.replace('a','@')
+rewrite = rewrite.replace('m','M')
+rewrite = rewrite.replace('B','8')
+rewrite = rewrite.replace('o','.')
+rewrite = rewrite + 'q*s'
+print(rewrite)
+
